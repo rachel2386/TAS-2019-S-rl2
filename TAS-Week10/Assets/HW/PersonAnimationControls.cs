1ebb1202bfea7c6fb47e21803cdde4dbbd81aa1f
rachelqxl <rl2563@nyu.edu>
2019-04-21 21:08:04 -0400

    week 11 procedural animation

---
+++ TAS-Week10/Assets/HW/PersonAnimationControls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonAnimationControls : MonoBehaviour
+{
+    Animator myAnim;
+    // Start is called before the first frame update
+    private float IdleTime = 0f;
+    private float moveTime = 0f;
+    [Range(0.001f, 3f)] public float walkCycleTime;
+    [Range(0.001f, 10f)] public float moveMagnitude;
+    [Range(0.00f, 1f)] public float OverallMovementBlendMeter;
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IdleTime += Time.deltaTime;
+        myAnim.SetFloat("IdleTime", Mathf.Abs(Mathf.Sin(IdleTime)));
+        if(Input.GetKey(KeyCode.W))
+            myAnim.SetBool("Moving", true);
+        else
+            myAnim.SetBool("Moving", false);
+
+        if (myAnim.GetBool("Moving"))
+        {
+            walkCycleTime = 1f-(0.5f *OverallMovementBlendMeter);
+            moveMagnitude = 0.25f + (0.75f * OverallMovementBlendMeter);
+            moveTime += Mathf.PI * 2 *Time.deltaTime / walkCycleTime;
+            myAnim.SetFloat("WalkBlendX", Mathf.Sin(moveTime) * moveMagnitude);
+            myAnim.SetFloat("WalkBlendY", Mathf.Cos(moveTime) * moveMagnitude);
+        }
+
+    }
+}
---
+++ TAS-Week10/Assets/Inclass/AnimatorParameterController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorParameterController : MonoBehaviour
+{
+    private float walkRun_TreeVal_X;
+    private float walkRun_TreeVal_Y;
+
+    private float time;
+
+    private float idleTime;
+
+    private Animator _myAnimator;
+
+    [Header("Tuning Values")]
+    [Range(0.001f, 10.0f)] public float walkCycleTime;
+    [Range(0.00f, 1.00f)] public float walkRunMagnitude;
+
+    [Range(0.00f, 1.00f)] public float walkRunBlendTotal;
+
+
+    //[Range(0.001f, 10.0f)] public float stepsPerSecond; // 2 * (1/(walkCycleTime)
+
+    //Soh - opposite/hypotenuse
+    //Cah - adjacent/hypotenuse
+    //Toa - opposite/adjacent
+
+    private void Start()
+    {
+        _myAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            _myAnimator.SetBool("Moving", true);
+        else
+            _myAnimator.SetBool("Moving", false);
+
+        idleTime += Time.deltaTime * 6;
+        _myAnimator.SetFloat("IdleBlend", (Mathf.Sin(idleTime)+1)/2);
+  
+
+
+        walkCycleTime = 1 - (.5f * walkRunBlendTotal);
+        walkRunMagnitude = .25f + (.75f * walkRunBlendTotal);
+
+        time += (Mathf.PI * 2 * Time.deltaTime) / walkCycleTime;
+
+        walkRun_TreeVal_X = Mathf.Cos(time) * walkRunMagnitude;
+        walkRun_TreeVal_Y = Mathf.Sin(time) * walkRunMagnitude;
+
+        _myAnimator.SetFloat("WalkX", walkRun_TreeVal_X);
+        _myAnimator.SetFloat("WalkY", walkRun_TreeVal_Y);
+    }
+}
