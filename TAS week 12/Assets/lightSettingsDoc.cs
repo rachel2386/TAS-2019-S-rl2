c66c0b49f61f7b197eb6de478e48cdaca2cb291f
rachelqxl <rl2563@nyu.edu>
2019-04-29 01:18:18 -0400

    week 12 proj added

---
+++ TAS week 12/Assets/lightSettingsDoc.cs
@@ -0,0 +1,17 @@
+﻿//sunrise:
+//Camera Background Color: 84802C
+//Directional Light FFB900 intensity 3.69 shadow strength 0.59
+//Ambient Light: FF9500
+//Fog: 84802C density 0.13 exponential
+
+//mystery:
+//Camera Background Color: 7DE2DD
+//Directional Light 18587D intensity 3.69 shadow strength 1
+//Ambient Light: 437180
+//Fog: 2C8477 density 0.06 exponential
+
+//warm:
+//Camera Background Color: 17FFF6
+//Directional Light FFCE25 intensity 4.84 shadow strength 0.477
+//Ambient Light: C06700
+//Fog: 54FAB7 linear start -21.46 end 301.4
